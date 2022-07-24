31b7dfe382a9d6c660b7bc585ed02eecc25d4736
CintiaKarolliny <cintiakarolliny.9@gmail.com>
2022-07-24 02:48:17 -0300

    Projeto sobre lógica condicional, implementação dos exercícios sobre os assuntos

---
+++ exercOper.Relacionais/src/one/digitalinnovation/basecamp/Main.java
@@ -0,0 +1,12 @@
+package one.digitalinnovation.basecamp;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int i1 = 10;
+        int 12 = 20;
+        int 
+
+    }
+}
